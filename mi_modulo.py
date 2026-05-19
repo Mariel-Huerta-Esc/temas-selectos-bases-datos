fd831b88356d4091acb197d833fe1b9ca57422c4
Mariel Huerta Escobar <marielhuerta818@gmail.com>
2026-05-18 19:15:39 -0600

    ejercicios de funciones e importaciones

--- mi_modulo.py
+++ mi_modulo.py
@@ -5,3 +5,18 @@ def contar_letras(texto):
     texto_limpio = texto.strip()
     num_letras = len(texto_limpio)
     return num_letras
+
+def contar_nombres(texto):
+    palabras = texto.split()
+    return len(palabras)
+
+
+def contar_consonantes(texto):
+    consonantes = "bcdfghjklmnñpqrstvwxyz"
+    texto_minusc = texto.lower()
+    contador = 0
+    for caracter in texto_minusc:
+        if caracter in consonantes:
+            contador += 1
+            
+    return contador 
