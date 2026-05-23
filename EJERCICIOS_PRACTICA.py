a36ca84c6138285ffd341f6e8c374c6e9d448bb9
Mariel Huerta Escobar <marielhuerta818@gmail.com>
2026-05-22 18:05:38 -0600

    ejercicio 5 de la tarea

---
+++ EJERCICIOS_PRACTICA.py
@@ -0,0 +1,20 @@
+#diccionario
+alumnos = {
+    "Carlos" : 9,
+    "Maria" : 8.5,
+    "José" : 7.8,
+    "Pedro" : 9.8,
+    "Julio" : 7,
+    "Carolina" : 8.5,
+    "Pablo" : 7.6
+}
+valor_promedio = alumnos["Carlos"]
+print(valor_promedio)
+
+alumnos["Maria"] = 10 #modificar promedio
+
+print(alumnos)
+
+#añadir nuevo elemento
+alumnos["Karla"] = 7.8
+print(alumnos)
